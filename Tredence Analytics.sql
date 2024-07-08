00b90cec4bde51d379b15564f93d53552aba1e04
Aryan Monga <118589541+Aryan4433@users.noreply.github.com>
2024-07-08 17:30:14 +0530

    Add files via upload

---
+++ Tredence Analytics.sql
@@ -0,0 +1,38 @@
+use aryan_technology;
+CREATE TABLE cinema (
+    seat_id INT PRIMARY KEY,
+    free int
+);
+
+INSERT INTO cinema (seat_id, free) VALUES (1, 1);
+INSERT INTO cinema (seat_id, free) VALUES (2, 0);
+INSERT INTO cinema (seat_id, free) VALUES (3, 1);
+INSERT INTO cinema (seat_id, free) VALUES (4, 1);
+INSERT INTO cinema (seat_id, free) VALUES (5, 1);
+INSERT INTO cinema (seat_id, free) VALUES (6, 0);
+INSERT INTO cinema (seat_id, free) VALUES (7, 1);
+INSERT INTO cinema (seat_id, free) VALUES (8, 1);
+INSERT INTO cinema (seat_id, free) VALUES (9, 0);
+INSERT INTO cinema (seat_id, free) VALUES (10, 1);
+INSERT INTO cinema (seat_id, free) VALUES (11, 0);
+INSERT INTO cinema (seat_id, free) VALUES (12, 1);
+INSERT INTO cinema (seat_id, free) VALUES (13, 0);
+INSERT INTO cinema (seat_id, free) VALUES (14, 1);
+INSERT INTO cinema (seat_id, free) VALUES (15, 1);
+INSERT INTO cinema (seat_id, free) VALUES (16, 0);
+INSERT INTO cinema (seat_id, free) VALUES (17, 1);
+INSERT INTO cinema (seat_id, free) VALUES (18, 1);
+INSERT INTO cinema (seat_id, free) VALUES (19, 1);
+INSERT INTO cinema (seat_id, free) VALUES (20, 1);
+
+-- find consecutive seat availability
+
+select * from cinema;
+
+with cte as (
+select * ,row_number() over(order by seat_id) rn,
+seat_id - row_number() over(order by seat_id)  grp  from cinema where free=1)
+
+
+
+select seat_id from(select * ,count(*) over(partition by grp) cnt from cte) A where A.cnt>2;
